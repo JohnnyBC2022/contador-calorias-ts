26750a2431677e26d7aa0183f3819a6dd712889e
Johnny <jonathanbc2012@gmail.com>
2024-12-03 14:27:30 +0100

    feat: set-activeId added to reducer

--- src/reducers/activity-reducer.ts
+++ src/reducers/activity-reducer.ts
@@ -2,14 +2,17 @@ import { Activity } from './../types/index';
 
 
 export type ActivityActions =
-    { type: 'save-activity', payload: { newActivity: Activity } }
+    { type: 'save-activity', payload: { newActivity: Activity } } |
+    { type: 'set-activeId', payload: { id: Activity['id'] } }
 
 type ActivityState = {
-    activities: Activity[]
+    activities: Activity[],
+    activeId: Activity['id']
 }
 
 export const initialState: ActivityState = {
-    activities: []
+    activities: [],
+    activeId: ''
 }
 
 export const activityReducer = (
@@ -24,6 +27,13 @@ export const activityReducer = (
         return {
             ...state,
             activities: [...state.activities, action.payload.newActivity]
+        }
+    }
+
+    if(action.type === 'set-activeId') {
+        return {
+            ...state,
+            activeId: action.payload.id
         }
     }
 
