83b49b888534ffb24ba48442ab75cafde6068829
Johnny <jonathanbc2012@gmail.com>
2024-12-04 11:44:23 +0100

    feat: delete action added

--- src/reducers/activity-reducer.ts
+++ src/reducers/activity-reducer.ts
@@ -3,7 +3,8 @@ import { Activity } from './../types/index';
 
 export type ActivityActions =
     { type: 'save-activity', payload: { newActivity: Activity } } |
-    { type: 'set-activeId', payload: { id: Activity['id'] } }
+    { type: 'set-activeId', payload: { id: Activity['id'] } } |
+    { type: 'delete-activity', payload: { id: Activity['id'] } }
 
 export type ActivityState = {
     activities: Activity[],
@@ -40,6 +41,13 @@ export const activityReducer = (
         return {
             ...state,
             activeId: action.payload.id
+        }
+    }
+
+    if (action.type === 'delete-activity') {
+        return {
+            ...state,
+            activities: state.activities.filter(activity => activity.id !== action.payload.id),
         }
     }
 
