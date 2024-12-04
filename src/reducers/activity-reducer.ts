3946841996afaaf9846cb22913cc405d385189af
Johnny <jonathanbc2012@gmail.com>
2024-12-04 11:55:57 +0100

    feat: localstorage added

--- src/reducers/activity-reducer.ts
+++ src/reducers/activity-reducer.ts
@@ -6,13 +6,20 @@ export type ActivityActions =
     { type: 'set-activeId', payload: { id: Activity['id'] } } |
     { type: 'delete-activity', payload: { id: Activity['id'] } }
 
+
+
 export type ActivityState = {
     activities: Activity[],
     activeId: Activity['id']
 }
 
+const localStorageActivities = (): Activity[]=>{
+    const activities = localStorage.getItem('activities');
+    return activities ? JSON.parse(activities) : [];
+}
+
 export const initialState: ActivityState = {
-    activities: [],
+    activities: localStorageActivities(),
     activeId: ''
 }
 
